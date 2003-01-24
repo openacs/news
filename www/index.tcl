52558de1992a46eaa7908917b29823c9e15dd20b
Dave Bauer <dave@thedesignexperience.org>
2003-01-24 02:07:11 +0000

    Use site_node_closest_ancestor_package_url to find closest "search" package instance. Fixes Bug#162

--- www/index.tcl
+++ www/index.tcl
@@ -41,8 +41,8 @@ set news_create_p [ad_permission_p $package_id news_create]
 
 
 # switch for showing interface to site-wide-search for news
-set allow_search_p [ad_parameter ShowSearchInterfaceP "news" 1]
-set search_url [apm_package_url_from_key search]
+set allow_search_p [parameter::get -package_id $package_id -parameter ShowSearchInterfaceP  -default 1]
+set search_url [site_node_closest_ancestor_package_url search]
 
 # view switch in live | archived news
 if { [string equal "live" $view] } {
