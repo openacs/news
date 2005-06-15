95749cf82298e25b63f8bdde422ced4726f3994f
Enrique Catalan <ec@ecatalan.com>
2005-06-15 17:50:27 +0000

    Removing merge news implementations

--- tcl/news-procs.tcl
+++ tcl/news-procs.tcl
@@ -372,39 +372,3 @@ ad_proc -private news_update_rss {
     rss_gen_report $subscr_id
 }
 
-namespace eval news::merge {
-
-    ad_proc -callback MergeShowUserInfo -impl news {
-	-user_id:required
-    } {
-	Show the news items 
-    } {
-	set msg "News items of $user_id"
-	ns_log Notice $msg
-	set result [list $msg]
-
-	set news [db_list_of_lists getaprovednews { *SQL* }]
-
-	lappend result $news
-
-	return $result
-    }
-
-    ad_proc -callback MergePackageUser -impl news {
-	-from_user_id:required
-	-to_user_id:required
-    } {
-	Merge the news of two users.
-    } {
-	set msg "Merging news"
-	ns_log Notice $msg
-	set result [list $msg]
-
-	db_dml update_from_news_approval { *SQL* }
-
-	lappend result "Merge of news is done"
-
-	return $result
-    }
-
-}
