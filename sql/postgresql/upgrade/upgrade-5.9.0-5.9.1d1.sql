cb8632d71888144ff3a9ac6b87ec89ddd220ce2b
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2016-08-11 13:03:47 +0000

    - clean dirty editor buffer

--- sql/postgresql/upgrade/upgrade-5.9.0-5.9.1d1.sql
+++ sql/postgresql/upgrade/upgrade-5.9.0-5.9.1d1.sql
@@ -2,7 +2,8 @@
 -- Cleanup cr_revisions belonging to the news package, for which there
 -- are no news_id in cr_news. These entries were created since ever in
 -- the news package. The bug adding spurious revisions was fixed with
--- the OpenACS 5.9.0 release, this upgrade scripts removes leftovers.
+-- the OpenACS 5.9.0 release, this upgrade scripts removes leftovers
+-- of the earlier bug.
 --
 select revision_id, content_revision__delete(revision_id) from 
   (select revision_id from cr_revisions where item_id in (
