08256b01e9c1eb284ae765f1784a61dd07711dc5
Antonio Pisano <antonio@elettrotecnica.it>
2019-12-30 17:58:04 +0000

    Use ad_inlcude_contract

--- www/news.tcl
+++ www/news.tcl
@@ -1,5 +1,16 @@
-# /packages/news/www/news.tcl
-# Display one news revision.
+ad_include_contract {
+    /packages/news/www/news.tcl
+
+    Display one news revision.
+} {
+    item_id:naturalnum,optional
+    publish_title:optional
+    publish_lead:html,optional
+    publish_image:localurl,optional
+    publish_body:html
+    creator_link:html
+    publish_format:notnull
+}
 
 set publish_body [ad_html_text_convert -from $publish_format -to text/html -- $publish_body]
 
