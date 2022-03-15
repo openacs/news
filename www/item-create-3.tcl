dc8dab33d0ea990219bd117c76fdae1dde10e275
Antonio Pisano <antonio@elettrotecnica.it>
2022-03-15 15:56:30 +0000

    Fix more boolean expressions

--- www/item-create-3.tcl
+++ www/item-create-3.tcl
@@ -36,7 +36,7 @@ set approval_policy [parameter::get -parameter ApprovalPolicy -default "wait"]
 #
 # the news_admin or an open approval policy allow immediate publishing
 #
-if { $news_admin_p == 1 || $approval_policy eq "open" } {
+if { $news_admin_p || $approval_policy eq "open" } {
     set approval_user [ad_conn user_id]
     set approval_ip [ad_conn peeraddr]
     set approval_date [dt_sysdate]
@@ -49,7 +49,7 @@ if { $news_admin_p == 1 || $approval_policy eq "open" } {
 }
 
 # Allow the user to "never expire" a news item.
-if {$permanent_p == "t"} {
+if {$permanent_p} {
     set archive_date_ansi ""
 }
 
